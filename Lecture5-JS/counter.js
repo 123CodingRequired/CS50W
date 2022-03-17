c5503de793dd8b533341bc9a59d15a68faca50f1
123CodingRequired <123CodingRequired@gmail.com>
2022-03-17 12:55:42 -0700

    add lec4

---
+++ Lecture5-JS/counter.js
@@ -0,0 +1,35 @@
+// let counter = 0
+//         function count(){
+//             counter++
+//             document.querySelector('h1').innerHTML = counter
+
+//             if(counter % 10 === 0) {
+//                 alert(`Count is now ${counter}`)
+//             }
+//         }
+//         document.addEventListener('DOMContentLoaded', function(){
+//             document.querySelector('button').onclick = count
+//         }) 
+// Check if there is already a vlaue in local storage
+if (!localStorage.getItem('counter')) {
+
+    // If not, set the counter to 0 in local storage
+    localStorage.setItem('counter', 0);
+}
+            
+function count() {
+    // Retrieve counter value from local storage
+    let counter = localStorage.getItem('counter');
+
+    // update counter
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+
+    // Store counter in local storage
+    localStorage.setItem('counter', counter);
+}
+
+
+// Set heading to the current value inside local storage
+document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+document.querySelector('button').onclick = count;
